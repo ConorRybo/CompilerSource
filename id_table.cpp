75452a61332544402f2e3e90fbe7f3b2dfe99dba
Conor Rybo <conor.rybo@gmail.com>
2021-11-29 00:46:30 -0500

    gettting some broad thoughts down on what I need to implement

--- id_table.cpp
+++ id_table.cpp
@@ -1,8 +1,8 @@
 /*
  * idtable.cpp
  *
- *  Created on: Jun 18, 2020
- *      Author: Michael Oudshoorn
+ *  Modified on: November 29, 2020
+ *      Author: Conor Rybacki
  */
 
 
@@ -16,6 +16,20 @@
 
 using namespace std;
 
+/*
+	NEEDS:
+		- Private variable to refer to an entry in the id table
+		- private struct id_entry
+			... what information should it contain?
+		- enter_new_scope() : used when we are entering a new level of scope...
+		... what does this need to do?
+		- exit_scope() : exiting (moving up?) a scope level...
+		... how do we manage this?
+		- lookup() : used to look up if an identifier exists in the table
+		... if it doesn't - insert() : insert the identifier into the table
+		- fix_up() : if there are multiple variables being declared at the same time
+			we must then go back and set their types after they are all recognized
+*/
 
 id_table::id_table(error_handler* err)
 {
